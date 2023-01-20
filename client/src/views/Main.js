f0f7665e3861234457e53484750b52fc57319b93
MedinaZS <101607623+MedinaZS@users.noreply.github.com>
2023-01-19 23:40:36 -0300

    First Commit

---
+++ client/src/views/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ProductCreateForm from '../components/ProductCreateForm'
+
+const Main = () => {
+  return (
+    <ProductCreateForm />
+  )
+}
+
+export default Main
